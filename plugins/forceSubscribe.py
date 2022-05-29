df3097727ee19c09e02add7d3e71915fe213bda5
mkskopaing123 <102409234+mkskopaing123@users.noreply.github.com>
2022-05-29 21:51:11 +0630

    Update forceSubscribe.py

--- plugins/forceSubscribe.py
+++ plugins/forceSubscribe.py
@@ -54,7 +54,7 @@ async def _check_member(client, message):
       except UserNotParticipant:
         try:
           sent_message = await message.reply_text(
-              " {} ရေ , မင်မင်တိုရဲ့ ချန်နယ်ကို အခုထိ subscribed မလုပ်ရသေးပါဘူ:။  ဒီအောက်က MKS Main Channel (@MKSVIPLINK) ကိုနှိပ်ပြီး join ပေးပါဦး ။          Join ပြီးရင် unmute me  ကို နှိပ်လိုက်ပါဗျာ။ ပုံမှန်တိုင်း စာပြန်ရိုက်လိုရပါလိမ့်မယ်ဗျာ။ကျေးဇူးတင်ပါတယ်❣️ .".format(message.from_user.mention, channel, channel),
+              " {} ရေ , မင်မင်တိုရဲ့ ချန်နယ်ကို အခုထိ subscribed မလုပ်ရသေးပါဘူ:။                                            ဒီအောက်က MKS Main Channel (@MKSVIPLINK) ကိုနှိပ်ပြီး join ပေးပါဦး ။                                                     Join ပြီးရင် unmute me  ကို နှိပ်လိုက်ပါဗျာ။ ပုံမှန်တိုင်း စာပြန်ရိုက်လိုရပါလိမ့်မယ်ဗျာ။ကျေးဇူးတင်ပါတယ်❣️ .".format(message.from_user.mention, channel, channel),
               disable_web_page_preview=True,
              reply_markup=InlineKeyboardMarkup(
             [
