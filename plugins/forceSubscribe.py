d897b53c00e00d42b872a247900e7f9f770438ad
mkskopaing123 <102409234+mkskopaing123@users.noreply.github.com>
2022-06-09 11:38:47 +0630

    Update forceSubscribe.py

--- plugins/forceSubscribe.py
+++ plugins/forceSubscribe.py
@@ -54,18 +54,18 @@ async def _check_member(client, message):
       except UserNotParticipant:
         try:
           sent_message = await message.reply_text(
-              " {} ရေ , မင်မင်တိုရဲ့ ချန်နယ်ကို အခုထိ subscribed မလုပ်ရသေးပါဘူ:။                                            ဒီအောက်က MKS Main Channel (@MKSVIPLINK) ကိုနှိပ်ပြီး join ပေးပါဦး ။                                                     Join ပြီးရင် unmute me  ကို နှိပ်လိုက်ပါဗျာ။ ပုံမှန်တိုင်း စာပြန်ရိုက်လိုရပါလိမ့်မယ်ဗျာ။ကျေးဇူးတင်ပါတယ်❣️ .".format(message.from_user.mention, channel, channel),
+              " {} ရေ , မင်မင်တိုရဲ့ ချန်နယ်ကို အခုထိ subscribed မလုပ်ရသေးပါဘူ:။                                            ဒီအောက်က ❣️ MKS Main Channel ❣️(@MKSVIPLINK) ကိုနှိပ်ပြီး join ပေးပါဦး ။                                                     Join ပြီးရင် unmute me  ကို နှိပ်လိုက်ပါဗျာ။ ပုံမှန်တိုင်း စာပြန်ရိုက်လိုရပါလိမ့်မယ်ဗျာ။ကျေးဇူးတင်ပါတယ်❣️ .".format(message.from_user.mention, channel, channel),
               disable_web_page_preview=True,
              reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("MKS Main Channel", url=channel_url)
+                    InlineKeyboardButton("❣️ MKS Main Channel ❣️", url=channel_url)
                 ],
                 [
-                    InlineKeyboardButton("UnMute Me", callback_data="onUnMuteRequest")
+                    InlineKeyboardButton("❣️ UnMute Me ❣️", callback_data="onUnMuteRequest")
                 ],
                 [
-                    InlineKeyboardButton("KP Main Channel", url=)
+                    InlineKeyboardButton("❣️ KP World Movie Channel ❣️", url="https://t.me/kpmovielist")
                 ]
             ]
         )
